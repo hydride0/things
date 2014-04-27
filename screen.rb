d104ffa69e84bebbdf6595571a0cd3ba4844de0b
hydride0 <erkiwi@mail.com>
2014-04-27 15:16:48 +0200

    Remove bullshits

--- screen.rb
+++ screen.rb
@@ -2,10 +2,6 @@
 # screenfetch clone for Arch
 
 class String
-	def to(col)
-		self.color(col).bold
-	end
-  
 	def xtr(hash)
 		self.match(/(.+):(.+)/) { |m| hash.store( m[1].strip, m[2].strip ) }
 		return hash
@@ -19,22 +15,22 @@ info = {}
 `xdpyinfo | grep 'dimensions'`.strip.xtr(info)
 `wmctrl -m | grep 'Name'`.strip.xtr(info)
 
-puts "                   -`".to(:cyan)
-puts "                  .o+`                 ".to(:cyan) + "Host: ".to(:cyan) + `hostname`
-puts "                 ´ooo/                 ".to(:cyan) + "OS: ".to(:cyan) + `lsb_release -si`
-puts "                ´xoooo:                ".to(:cyan) + "Kernel: ".to(:cyan) +`uname -sr`
-puts "               ´xoooooo:               ".to(:cyan) + "Uptime: ".to(:cyan) + `uptime -p`
-puts "               -xoooooo+:              ".to(:cyan) + "WM: ".to(:cyan) + info["Name"]
-puts "             ´/:-:++oooo+:             ".to(:cyan) + "Shell: ".to(:cyan) + `echo $SHELL`
-puts "            ´/++++/+++++++:            ".to(:cyan) + "Editor: ".to(:cyan) + `echo $EDITOR`
-puts "           ´/++++++++++++++:           ".to(:cyan) + "CPU: ".to(:cyan) + info["model name"]
-puts "          ´/+++ooooooooooooo/`         ".to(:cyan) + "Architecture: ".to(:cyan) + `uname -m`
-puts "         ./ooosssso++osssssso+`        ".to(:cyan) + "Resolution: ".to(:cyan) + info["dimensions"]
-puts "        .oossssso-````/ossssss+`       ".to(45)
-puts "       -osssssso.      :ssssssso.      ".to(45)
-puts "      :osssssss/        osssso+++.     ".to(45)
-puts "     /ossssssss/        +ssssooo/-     ".to(45)
-puts "   ´/ossssso+/:-        -:/+osssso+-   ".to(45)
-puts "  ´+sso+:-`                 `.-/+oso:  ".to(45)
-puts " ´++:.                           `-/+/ ".to(45)
-puts " .`                                 `/ ".to(45)
+puts "                   -`".cyan
+puts "                  .o+`                 ".cyan + "Host: ".cyan + `hostname`
+puts "                 ´ooo/                 ".cyan + "OS: ".cyan + `lsb_release -si`
+puts "                ´xoooo:                ".cyan + "Kernel: ".cyan + `uname -sr`
+puts "               ´xoooooo:               ".cyan + "Uptime: ".cyan + `uptime -p`
+puts "               -xoooooo+:              ".cyan + "WM: ".cyan + info["Name"]
+puts "             ´/:-:++oooo+:             ".cyan + "Shell: ".cyan + `echo $SHELL`
+puts "            ´/++++/+++++++:            ".cyan + "Editor: ".cyan + `echo $EDITOR`
+puts "           ´/++++++++++++++:           ".cyan + "CPU: ".cyan + info["model name"]
+puts "          ´/+++ooooooooooooo/`         ".cyan + "Architecture: ".cyan + `uname -m`
+puts "         ./ooosssso++osssssso+`        ".cyan + "Resolution: ".cyan + info["dimensions"]
+puts "        .oossssso-````/ossssss+`       ".color(45)
+puts "       -osssssso.      :ssssssso.      ".color(45)
+puts "      :osssssss/        osssso+++.     ".color(45)
+puts "     /ossssssss/        +ssssooo/-     ".color(45)
+puts "   ´/ossssso+/:-        -:/+osssso+-   ".color(45)
+puts "  ´+sso+:-`                 `.-/+oso:  ".color(45)
+puts " ´++:.                           `-/+/ ".color(45)
+puts " .`                                 `/ ".color(45)
